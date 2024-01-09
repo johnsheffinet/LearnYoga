02c4d707b679dc8f4da8da1306d4597ffc907456
John William Sheffield Jr <john@sheffinet.org>
2024-01-09 02:22:53 +0000

    simple mutation exercise

--- src/schema.ts
+++ src/schema.ts
@@ -37,7 +37,7 @@ const resolvers = {
     feed: () => links,
   },
   Mutation: {
-    createLink: (_, args: {url: string, description: string}) => {
+    createLink: (parent: unknown, args: {url: string, description: string}) => {
       const link: Link = {
         id: `link-${links.length}`,
         url: args.url,
