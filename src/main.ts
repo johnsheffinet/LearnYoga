d0a67491808725533a02507353cf57667bc50dd7
John William Sheffield Jr <john@sheffinet.org>
2024-01-24 05:43:00 +0000

    promises

--- src/main.ts
+++ src/main.ts
@@ -32,4 +32,85 @@ function main() {
   });
 }
 
+//main();
+
+/*
+const addNumbers = (a: number, b: number) => {
+    
+  return new Promise((resolve, reject) => {
+    
+    setTimeout(() => {
+      resolve(a + b);
+    }, 1000);
+  
+  });
+  
+  
+};
+
+let getResult = async (a: number, b: number) => {
+    let value = await addNumbers(a, b);
+    console.log(value);
+};
+
+getResult(3, 3);
+
+*/
+function f(x: any, y: any) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (x === y) {
+        resolve(`${x} === ${y}`);
+      } else {
+        reject(`${x} !== ${y}`);
+      }
+    }, 3000);
+  });
+}
+
+console.log("Starting ...");
+f(0, 0)
+  .then((s) => {
+    console.log(s);
+  })
+  .catch((s) => {
+    console.error(s);
+  })
+  .finally(() => {
+    console.log("Done!");
+  });
+
+console.log("Starting ...");
+async function g(x: any, y: any) {
+  try {
+    const s = await f(x, y);
+    console.log(s);
+  } catch (s) {
+    console.error(s);
+  } finally {
+    console.log("Done!");
+  }
+}
+
+g(0, "0");
+/*
+import { schema } from "./schema";
+import { execute, parse } from "graphql";
+
+async function main() {
+  const myQuery = parse(`
+    query {
+      hello
+    }
+  `);
+
+  const result = await execute({
+    schema,
+    document: myQuery,
+  });
+
+  console.log(result);
+}
+
 main();
+*/
